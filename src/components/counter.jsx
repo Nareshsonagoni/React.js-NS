5fd44d8ce81eee67d823fbdccd9c390c77a27003
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-25 14:29:14 +1100

    conditonal rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,17 +4,23 @@ class Counter extends Component {
   state = {
     // we use state to define all the elements used in the present component.
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: [] //["tag1", "tag2", "tag3"]
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+    return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
+  }
   render() {
     return (
       <React.Fragment>
         <span className={this.getBedgeClass()}>{this.formatCount()}</span>
         <button className="badge badge-secondary btn-sm">Increament</button>
         <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
+          <span>{this.state.tags.length === 0 && "Please add some tags"}</span>
+          {/* // when JS evaluates this expression it will return the last conditon always if true.*/}
+          {this.renderTags()}
         </ul>
       </React.Fragment>
     );
